7362b7af710962292b98fe346e80e9386b26a084
xulihang <xulihanghai@163.com>
2023-03-17 13:50:45 +0800

    add python backend

---
+++ python/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, send_file
+from PIL import Image
+import base64
+from io import BytesIO
+import base64
+app = Flask(__name__)
+
+@app.route('/Upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'base64' in data:
+            path = './uploaded/'
+            if os.path.exists(path)==False:
+                os.makedirs(path)
+            bytes_decoded = base64.b64decode(data['base64'])
+            img = Image.open(BytesIO(bytes_decoded))
+            # to jpg
+            out_jpg = img.convert('RGB')
+            filename = str(int(time.time()*1000))+'.jpg'
+            # save file
+            out_jpg.save(os.path.join(path,filename))
+            response={'status': 'success', 'filename': filename}
+            return response
+        
+@app.route('/Get', methods=['GET'])
+def get():
+    filename = request.args.get('filename', '')
+    path = os.path.join('./uploaded/',filename)
+    if os.path.exists(path):
+        return send_file(path,as_attachment=True, attachment_filename=filename)
+
+
+if __name__ == '__main__':
+   app.run(host = "0.0.0.0", port = 8888, ssl_context='adhoc')
